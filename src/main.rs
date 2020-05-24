a656d10f29e6ea95a07c3b1ca8385a5a7a90315a
ocha <ochaochaocha3@gmail.com>
2020-05-25 00:45:53 +0900

    println!をいろいろ追加してみる

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+    println!("Hello, {}!", "ocha");
+
+    let a: f64 = 1.0;
+    let b: f64 = 2f64;
+    println!("{:.1} + {:.1} = {:.1}", a, b, add(a, b));
+
+    println!(
+        "半径 {:.1}、円周率 {:.3}、面積 {:.3}",
+        3.2,
+        std::f64::consts::PI,
+        3.2f64.powi(2) * std::f64::consts::PI,
+    )
+}
+
+fn add(x: f64, y: f64) -> f64 {
+    x + y
 }
